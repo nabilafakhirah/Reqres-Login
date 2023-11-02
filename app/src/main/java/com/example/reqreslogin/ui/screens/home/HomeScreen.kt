b0d033dfd01e1789fe7ae03caf4dda05ac618614
Nabila Fakhirah <nabilafakhirah@gmail.com>
2023-11-02 08:54:32 +0700

    Added logout

--- app/src/main/java/com/example/reqreslogin/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/reqreslogin/ui/screens/home/HomeScreen.kt
@@ -1,13 +1,20 @@
 package com.example.reqreslogin.ui.screens.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -15,8 +22,12 @@ import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import com.example.reqreslogin.R
 import com.example.reqreslogin.data.model.UserResponse
+import com.example.reqreslogin.ui.navigation.Destinations
 import com.example.reqreslogin.ui.screens.home.widget.UserItemView
+import com.example.reqreslogin.utils.PreferencesManager
+import com.example.reqreslogin.utils.TOKEN_KEY
 
 @Composable
 fun HomeScreen(
@@ -26,12 +37,27 @@ fun HomeScreen(
 ) {
     val state = viewModel.state
     val userList = state.value.userList?.collectAsLazyPagingItems()
-
+    val context = LocalContext.current
+    val preferencesManager = remember { PreferencesManager(context = context) }
     Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 16.dp)
+                .padding(top = 16.dp)
         ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 8.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                modifier = Modifier.clickable {
+                    preferencesManager.deleteData(TOKEN_KEY)
+                    navController.navigate(Destinations.LOGIN_PAGE_ROUTE)
+                },
+                painter = painterResource(id = R.drawable.ic_logout),
+                contentDescription = "")
+        }
         if (userList != null) {
             ListContent(
                 users = userList,
